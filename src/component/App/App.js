454bf7bea27eca205a0b4dc0fadef27eca4145fd
Arnold Balliu <arnold.balliu@gmail.com>
2017-12-25 14:14:46 -0500

    test commit

--- src/component/App/App.js
+++ src/component/App/App.js
@@ -6,7 +6,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        This is component App
+        This is component App. Hello!
       </div>
     );
   }
