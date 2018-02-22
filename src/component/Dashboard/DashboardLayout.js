fe0a70643036901e854e710e9b5518d1094c6375
Arnold Balliu <arnold.balliu@gmail.com>
2018-02-22 13:23:30 -0500

    markers and table finally updating - added timeline buttons - added current time ticker - investigating slow sidebar animation

--- src/component/Dashboard/DashboardLayout.js
+++ src/component/Dashboard/DashboardLayout.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
-import { Sidebar, Segment, Button, Table, Checkbox, Popup, Form, Header, Divider, Dimmer, Loader } from 'semantic-ui-react'
+import { Sidebar, Segment, Button, Table, Checkbox, Popup, Form, Header, Divider, Dimmer, Loader, Grid, Label, Icon } from 'semantic-ui-react'
 import { withScriptjs, withGoogleMap, GoogleMap } from "react-google-maps"
 import Marker from 'react-google-maps/lib/components/Marker'
 import MarkerClusterer from 'react-google-maps/lib/components/addons/MarkerClusterer'
 import { DateRange } from 'react-date-range'
-import axios from 'axios'
+//import axios from 'axios'
+import request from 'superagent'
 import AppStatics from '../../helpers/AppStatics'
 import Momentjs from 'moment'
 
@@ -31,43 +32,43 @@ const BALTIMORE_LONG = -76.609383
 //     return text;
 // }
 
-function generateMarkers(jsonObjectArray){
+function generateMarkers(jsonObjectArray) {
     var markers = []
-    for (var i = 0; i<jsonObjectArray.length; i++){
+    for (var i = 0; i < jsonObjectArray.length; i++) {
         var call_record = jsonObjectArray[i]
-        if (!(typeof call_record === 'undefined') || !(typeof call_record.bpd_call_id === 'undefined')){
-            var marker = <Marker key={call_record.bpd_call_id} position={{ lat: call_record.latitude, lng: call_record.longitude}} />
+        if (!(typeof call_record === 'undefined') || !(typeof call_record.bpd_call_id === 'undefined')) {
+            var marker = <Marker key={call_record.bpd_call_id} position={{ lat: call_record.latitude, lng: call_record.longitude }} />
             markers.push(marker)
         }
     }
     return markers
 }
 
-function generateTableRows(jsonObjectArray){
+function generateTableRows(jsonObjectArray) {
     var rows = []
-    for (var i = 0; i<jsonObjectArray.length; i++){
+    for (var i = 0; i < jsonObjectArray.length; i++) {
         var call_record = jsonObjectArray[i];
-        if (!(typeof call_record === 'undefined') || !(typeof call_record.bpd_call_id === 'undefined')){
-            var row = 
-            (<Table.Row key={"table_row_"+call_record.bpd_call_id}>
-                <Table.Cell key={"table_cell_" + call_record.bpd_call_id + "_bpd_call_id"}>{call_record.bpd_call_id}</Table.Cell>
-                <Table.Cell key={"table_cell_" + call_record.bpd_call_id + "_time"}>{call_record.call_time}</Table.Cell>
-                <Table.Cell key={"table_cell_" + call_record.bpd_call_id + "_priority"}>{call_record.priority}</Table.Cell>
-                <Table.Cell key={"table_cell_" + call_record.bpd_call_id + "_district"}>{call_record.district}</Table.Cell>
-                <Table.Cell key={"table_cell_" + call_record.bpd_call_id + "_desc"}>{call_record.description}</Table.Cell>
-                <Table.Cell key={"table_cell_" + call_record.bpd_call_id + "_addr"}>{call_record.address}</Table.Cell>
-                <Table.Cell key={"table_cell_" + call_record.bpd_call_id + "_lat"}>{call_record.latitude}</Table.Cell>
-                <Table.Cell key={"table_cell_" + call_record.bpd_call_id + "_long"}>{call_record.longitude}</Table.Cell>
-            </Table.Row>);
+        if (!(typeof call_record === 'undefined') || !(typeof call_record.bpd_call_id === 'undefined')) {
+            var row =
+                (<Table.Row key={"table_row_" + call_record.bpd_call_id}>
+                    <Table.Cell key={"table_cell_" + call_record.bpd_call_id + "_bpd_call_id"}>{call_record.bpd_call_id}</Table.Cell>
+                    <Table.Cell key={"table_cell_" + call_record.bpd_call_id + "_time"}>{call_record.call_time}</Table.Cell>
+                    <Table.Cell key={"table_cell_" + call_record.bpd_call_id + "_priority"}>{call_record.priority}</Table.Cell>
+                    <Table.Cell key={"table_cell_" + call_record.bpd_call_id + "_district"}>{call_record.district}</Table.Cell>
+                    <Table.Cell key={"table_cell_" + call_record.bpd_call_id + "_desc"}>{call_record.description}</Table.Cell>
+                    <Table.Cell key={"table_cell_" + call_record.bpd_call_id + "_addr"}>{call_record.address}</Table.Cell>
+                    <Table.Cell key={"table_cell_" + call_record.bpd_call_id + "_lat"}>{call_record.latitude}</Table.Cell>
+                    <Table.Cell key={"table_cell_" + call_record.bpd_call_id + "_long"}>{call_record.longitude}</Table.Cell>
+                </Table.Row>);
             rows.push(row)
         }
     }
     return rows;
 }
 
-function generateTableHeader(array){
+function generateTableHeader(array) {
     var header_cells = []
-    for (var i = 0; i < array.length; i++){
+    for (var i = 0; i < array.length; i++) {
         var header_cell = (<Table.HeaderCell key={"table_header_cell_" + array[i]}>{array[i]}</Table.HeaderCell>)
         header_cells.push(header_cell)
     }
@@ -92,7 +93,7 @@ const MyMapComponent = withScriptjs(withGoogleMap((props) =>
             enableRetinaIcons
             gridSize={60}
         >
-        {generateMarkers(props.call_records)}
+            {generateMarkers(props.call_records)}
         </MarkerClusterer>
 
     </GoogleMap>
@@ -181,32 +182,31 @@ class DashboardLayout extends Component {
         headers: { "Content-Type": 'application/json' }
     };
 
-    axiosInstance = axios.create(this.axiosOptions)
+    axiosInstance//axios.create(this.axiosOptions)
 
     constructor(props) {
         super(props);
-        this.state = { visibleSidebar: false, call_records: [], isMarkerShown: false, showLoaders: true };
+        this.state = { visibleSidebar: false, call_records: [], isMarkerShown: false, showLoaders: true, currentTime: Momentjs().toString() };
     }
 
     componentDidMount() {
-        //this.setState({ showLoaders: false })
-        var instance = this
-        // this.axiosInstance.interceptors.request.use(function (config) {
-        //     // Do something before request is sent
-        //     console.log(config)
-        //     return config;
-        // }, function (error) {
-        //     // Do something with request error
-        //     return Promise.reject(error);
-        // });
-        this.axiosInstance.post('/records/search', {
+        setInterval(() => this.handleCurrentTime(), 1000)
+        this.fetchCallRecordsMadeToday(this)
+    }
 
-            'start_date': /*"2017-11-28"*/Momentjs().toString(),
-            'end_date': /*"2017-11-29"*/Momentjs().toString()
-        })
-            .then(response => {
-                console.log(response.data)
-                this.setState({ call_records: response.data, showLoaders: false });
+    fetchCallRecordsMadeToday(instance) {
+        request
+            .post(AppStatics.API_BASE_URL + '/records/search')
+            .send({
+
+                'start_date': Momentjs().toString(),
+                'end_date': Momentjs().toString()
+            })
+            .set('Accept', 'application/json')
+            .set('Content-Type', 'application/json')
+            .then(function (response) {
+                console.log(response.body.data)
+                instance.setState({ call_records: response.body.data, showLoaders: false })
             })
             .catch(function (error) {
                 instance.setState({ showLoaders: false })
@@ -214,54 +214,77 @@ class DashboardLayout extends Component {
             });
     }
 
-    componentDidCatch(error, errorInfo){
+    fetchCallRecordsMadeYesterday() {
+        var instance = this
+        request
+            .post(AppStatics.API_BASE_URL + '/records/search')
+            .send({
+
+                'start_date': Momentjs().add(-1, 'days').toString(),
+                'end_date': Momentjs().add(-1, 'days').toString()
+            })
+            .set('Accept', 'application/json')
+            .set('Content-Type', 'application/json')
+            .then(function (response) {
+                console.log(response.body.data)
+                instance.setState({ call_records: response.body.data, showLoaders: false })
+            })
+            .catch(function (error) {
+                instance.setState({ showLoaders: false })
+                console.log(error)
+            });
+    }
+
+    fetchCallRecordsMadeLastSevenDays() {
+        var instance = this
+        request
+            .post(AppStatics.API_BASE_URL + '/records/search')
+            .send({
+
+                'start_date': Momentjs().add(-7, 'days').toString(),
+                'end_date': Momentjs().add(-7, 'days').toString()
+            })
+            .set('Accept', 'application/json')
+            .set('Content-Type', 'application/json')
+            .then(function (response) {
+                console.log(response.body.data)
+                instance.setState({ call_records: response.body.data, showLoaders: false })
+            })
+            .catch(function (error) {
+                instance.setState({ showLoaders: false })
+                console.log(error)
+            });
+    }
+
+    fetchCallRecordsMadeLastThirtyDays() {
+        var instance = this
+        request
+            .post(AppStatics.API_BASE_URL + '/records/search')
+            .send({
+
+                'start_date': Momentjs().add(-30, 'days').toString(),
+                'end_date': Momentjs().add(-30, 'days').toString()
+            })
+            .set('Accept', 'application/json')
+            .set('Content-Type', 'application/json')
+            .then(function (response) {
+                console.log(response.body.data)
+                instance.setState({ call_records: response.body.data, showLoaders: false })
+            })
+            .catch(function (error) {
+                instance.setState({ showLoaders: false })
+                console.log(error)
+            });
+    }
+
+    componentDidCatch(error, errorInfo) {
         this.setState({
             error: error,
             errorInfo: errorInfo
-          });
+        });
 
         console.log(error)
     }
-
-    handleMarkerClick = () => {
-
-    }
-
-    // customRenderRow = (callRecord, index) => (
-
-    //     // if (typeof callRecord.id === 'undefined')
-    //     //     return
-    //     // !(typeof callRecord === 'undefined' || typeof callRecord.id === 'undefined') ?
-    //     //     <Table.Row key={"table_row_" + callRecord.id}>
-    //     //         <Table.Cell>
-    //     //             <Checkbox key={"table_cell_checkbox_" + callRecord.id} />
-    //     //         </Table.Cell>
-    //     //         <Table.Cell key={"table_cell_" + callRecord.id + "_id"}>{callRecord.bpd_call_id}</Table.Cell>
-    //     //         <Table.Cell key={"table_cell_" + callRecord.id + "_time"}>{callRecord.call_time}</Table.Cell>
-    //     //         <Table.Cell key={"table_cell_" + callRecord.id + "_priority"}>{callRecord.priority}</Table.Cell>
-    //     //         <Table.Cell key={"table_cell_" + callRecord.id + "_district"}>{callRecord.district}</Table.Cell>
-    //     //         <Table.Cell key={"table_cell_" + callRecord.id + "_desc"}>{callRecord.description}</Table.Cell>
-    //     //         <Table.Cell key={"table_cell_" + callRecord.id + "_addr"}>{callRecord.address}</Table.Cell>
-    //     //         <Table.Cell key={"table_cell_" + callRecord.id + "_lat"}>{callRecord.latitude}</Table.Cell>
-    //     //         <Table.Cell key={"table_cell_" + callRecord.id + "_long"}>{callRecord.longitude}</Table.Cell>
-    //     //     </Table.Row>
-    //     //     : this.return
-
-    //     <Table.Row key={"table_row_" + index}>
-    //         {/* <Table.Cell>
-    //             <Checkbox key={"table_cell_checkbox_" + callRecord.bpd_call_id} />
-    //         </Table.Cell> */}
-    //         <Table.Cell key={"table_cell_" + callRecord.bpd_call_id + "_bpd_call_id"}>{callRecord.bpd_call_id}</Table.Cell>
-    //         <Table.Cell key={"table_cell_" + callRecord.bpd_call_id + "_time"}>{callRecord.call_time}</Table.Cell>
-    //         <Table.Cell key={"table_cell_" + callRecord.bpd_call_id + "_priority"}>{callRecord.priority}</Table.Cell>
-    //         <Table.Cell key={"table_cell_" + callRecord.bpd_call_id + "_district"}>{callRecord.district}</Table.Cell>
-    //         <Table.Cell key={"table_cell_" + callRecord.bpd_call_id + "_desc"}>{callRecord.description}</Table.Cell>
-    //         <Table.Cell key={"table_cell_" + callRecord.bpd_call_id + "_addr"}>{callRecord.address}</Table.Cell>
-    //         <Table.Cell key={"table_cell_" + callRecord.bpd_call_id + "_lat"}>{callRecord.latitude}</Table.Cell>
-    //         <Table.Cell key={"table_cell_" + callRecord.bpd_call_id + "_long"}>{callRecord.longitude}</Table.Cell>
-    //     </Table.Row>
-
-    // )
 
     handlePriorityChange = (event, data) => {
         if (data.checked) {
@@ -289,24 +312,20 @@ class DashboardLayout extends Component {
 
     handleFilterFormSubmit = () => {
         var instance = this
-        instance.setState({ showLoaders: true })
-        console.log({
-            'start_date': this.formStartDate,
-            'end_date': this.formEndDate,
-            'priorities': this.formPriorities,
-            'districts': this.formDistricts
-        });
-        this.axiosInstance.post('/records/search', {
+        request
+            .post(AppStatics.API_BASE_URL + '/records/search')
+            .send({
 
-            'start_date': this.formStartDate.split(' ')[0],
-            'end_date': this.formEndDate.split(' ')[0],
-            'priorities': this.formPriorities,
-            'districts': this.formDistricts
-
-        })
-            .then(response => {
-                console.log(response.data)
-                instance.setState({ call_records: response.data, showLoaders: false })
+                'start_date': this.formStartDate,
+                'end_date': this.formEndDate,
+                'priorities': this.formPriorities,
+                'districts': this.formDistricts
+            })
+            .set('Accept', 'application/json')
+            .set('Content-Type', 'application/json')
+            .then(function (response) {
+                console.log(response.body.data)
+                instance.setState({ call_records: response.body.data, showLoaders: false })
             })
             .catch(function (error) {
                 instance.setState({ showLoaders: false })
@@ -354,14 +373,18 @@ class DashboardLayout extends Component {
 
     toggleSidebarVisibility() {
         this.setState({ visibleSidebar: !this.state.visibleSidebar })
-    } 
-            
+    }
+
+    handleCurrentTime() {
+        this.setState({currentTime: Momentjs().toString()})
+    }
+
 
     render() {
         return (
             <div>
                 <Sidebar.Pushable as={Segment}>
-                    <Sidebar ref="sidebar_ref" animation='slide along' width='wide' visible={this.state.visibleSidebar} vertical='true'>
+                    <Sidebar ref="sidebar_ref" animation='uncover' width='wide' visible={this.state.visibleSidebar} vertical='true'>
                         <Form style={{ margin: 10 }} onSubmit={this.handleFilterFormSubmit.bind(this)}>
                             <Header as='h4'>Filters</Header>
                             <Divider horizontal>Date Range</Divider>
@@ -396,14 +419,36 @@ class DashboardLayout extends Component {
                     </Sidebar>
 
                     <Sidebar.Pusher>
-                        <Segment basic textAlign='left'>
-                            <Popup
-                                trigger={<Button onClick={this.toggleSidebarVisibility.bind(this)} icon='settings' />}
-                                content="Show sidebar"
-                                basic
-                            />
-                        </Segment>
-                        <Segment basic>
+                        <Grid fluid inverted padded>
+                            <Grid.Row color='black' >
+                                <Grid.Column width={12} textAlign='left'>
+                                    <Popup
+                                        trigger={<Button inverted onClick={this.toggleSidebarVisibility.bind(this)} icon='sidebar' />}
+                                        content="Show sidebar"
+                                        basic
+                                    />
+                                    <Button.Group inverted color='green'>
+                                        <Button active='true'>Today</Button>
+                                        <Button>Yesterday</Button>
+                                        <Button>Last 7 Days</Button>
+                                        <Button>Last 30 days</Button>
+                                    </Button.Group>
+                                </Grid.Column>
+                                <Grid.Column width={4} textAlign='right' verticalAlign='middle'>
+                                    <Label color='green' floated='right' size='medium'>
+                                        <Icon name='calendar' />
+                                        Current Date:
+                                        <Label.Detail>{this.state.currentTime}</Label.Detail>
+                                    </Label>
+                                </Grid.Column>
+                            </Grid.Row>
+                        </Grid>
+                        {/* <Segment fluid inverted vertical basic>
+                            
+
+                        </Segment> */}
+
+                        <Segment vertical basic>
                             <Dimmer active={this.state.showLoaders}>
                                 <Loader size='large'>Loading</Loader>
                             </Dimmer>
